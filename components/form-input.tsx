4f046b473cfcb21ff7db8c96492cadb685f4a7a3
itstrap1016 <itstrap1016@gmail.com>
2025-01-28 22:46:52 +0900

    25-01-28 study

--- components/form-input.tsx
+++ components/form-input.tsx
@@ -3,6 +3,7 @@ interface FormInputProps {
   placeholder: string;
   required: boolean;
   errors: string[];
+  name: string;
 }
 
 export default function FormInput({
@@ -10,17 +11,21 @@ export default function FormInput({
   placeholder,
   required,
   errors,
+  name,
 }: FormInputProps) {
   return (
     <div className="flex flex-col gap-2">
       <input
+        name={name}
         className="pl-2 bg-transparent rounded-md w-full h-10 focus:outline-none ring-2 focus:ring-4 ring-neutral-200 focus:ring-orange-500 border-none placeholder:text-neutral-400 transition"
         type={type}
         placeholder={placeholder}
         required={required}
       />
       {errors.map((error, index) => (
-        <span key={index} className="text-red-500 font-medium"></span>
+        <span key={index} className="text-red-500 font-medium">
+          {error}
+        </span>
       ))}
     </div>
   );
